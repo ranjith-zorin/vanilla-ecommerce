8f914fcc23ddea14c090d5a6b0c895898cf50f78
ranjith <ranjithzorin@gmail.com>
2026-09-04 19:17:11 +0530

    Carousel issue fixed

--- index.js
+++ index.js
@@ -1,44 +1,35 @@
-// function Carousel(btn) {
-let currenSlideId = 0;
+let currentSlideID = 1;
 
-sliderElement = document.getElementById("slider");
+slider = document.getElementById("slider");
 
-totalSlides = sliderElement.childElementCount;
-console.log(totalSlides);
+totalSlide = slider.childElementCount;
 
-//   if (btn === "left") {
-// }
-
-//   } else if (btn === "right") {
-
+function prev() {
+  if (currentSlideID > 1) {
+    currentSlideID--;
+  }
+  console.log("prev-", currentSlideID);
+  showSlide();
+}
+function nxt() {
+  if (currentSlideID < totalSlide) {
+    currentSlideID++;
+    showSlide();
+    console.log("current-", currentSlideID);
+  }
+}
 function showSlide() {
-  slides = document.getElementById("slider").getElementsByTagName("li");
-  for (let index = 0; index < totalSlides; index++) {
+  slides = slider.getElementsByTagName("li");
+
+  console.log(slides);
+
+  for (let index = 0; index < totalSlide; index++) {
     const element = slides[index];
 
-    console.log("index", index);
-    console.log("slide id", currenSlideId);
-    if (currenSlideId == index + 1) {
+    if (currentSlideID === index + 1) {
       element.classList.remove("hidden");
-      console.log("executed!");
     } else {
       element.classList.add("hidden");
     }
   }
-}
-function nxt() {
-  if (currenSlideId < totalSlides) {
-    currenSlideId++;
-    console.log("nxt", currenSlideId);
-    showSlide();
-  }
-}
-function prev() {
-  if (currenSlideId > 1) {
-    currenSlideId--;
-    console.log("prev", currenSlideId);
-    showSlide();
-  }
-
-  //   }
 }
